6c0fc132a36d5c30a9d628f816f2cb560cb0d2ea
miseullang <miseullang@gmail.com>
2025-02-15 22:09:24 +0900

    :sparkles:Feature: 토스트메시지 컴포넌트 개발

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
-import Router from './routes/Route';
+import Router from './routes/Router';
+import { Toast } from '@components/Toast';
 
 function App() {
   return (
     <BrowserRouter>
+      <Toast />
       <Router />
     </BrowserRouter>
   );
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,13 +1,20 @@
 import LayeredButton from '@components/LayeredButton';
+import { useToastStore } from '@/store/useToastStore';
+import { useEffect } from 'react';
 
 const MainPage = () => {
+  const { showToast } = useToastStore();
+  useEffect(() => {
+    showToast('메인 페이지입니다.', 'error');
+  }
+  , [showToast]);
   return (
-    <>
+    <div className='h-screen bg-blue-100'>
       <div>MainPage</div>
       <LayeredButton theme='red' className='w-20 h-20'>확인</LayeredButton>
       <LayeredButton theme='gray'>메이트 취소</LayeredButton>
       <LayeredButton theme='blue'>방 구경하기</LayeredButton>
-    </>
+    </div>
   );
 };
 
