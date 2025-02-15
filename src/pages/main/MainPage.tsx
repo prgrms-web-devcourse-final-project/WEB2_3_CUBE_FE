36c51677183b2c834f982fcb9977f62f110e3dbf
miseullang <miseullang@gmail.com>
2025-02-15 18:10:28 +0900

    :lipstick:Style: 버튼 컴포넌트 퍼블리싱

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,10 +1,12 @@
-import EditList from '@components/EditList';
+import LayeredButton from '@components/LayeredButton';
 
 const MainPage = () => {
   return (
     <>
       <div>MainPage</div>
-      <EditList />
+      <LayeredButton theme='red' className='w-20 h-20'>확인</LayeredButton>
+      <LayeredButton theme='gray'>메이트 취소</LayeredButton>
+      <LayeredButton theme='blue'>방 구경하기</LayeredButton>
     </>
   );
 };
