7fef4bd6d109768cb9622b39357e8975202a3871
hyunw00 <khwland090@gmail.com>
2025-02-17 21:41:57 +0900

    💄 Style: 로그인 페이지 퍼블리싱

---
+++ src/pages/login/AgreeItem.tsx
@@ -0,0 +1,61 @@
+import classNames from 'classnames';
+
+import check_logo from '@assets/datalist/check-logo.svg';
+import toggle_up from '@assets/login/toggle-up.svg';
+
+export default function AgreeItem({
+  agreementInfo,
+  onToggleCheck,
+  onToggleContent,
+}) {
+  return (
+    <ul className={`relative z-[5]  ${agreementInfo.isExpanded && 'mb-31'} `}>
+      {/* 기본으로 보이는 부분 */}
+      <div className='item-between  py-4.5  bg-[#FFFFFF] rounded-2xl shadow-[0px_4px_4px_0px_rgba(78,122,207,0.15)]'>
+        <div className='item-middle gap-4 pl-10'>
+          <label className='item-middle  w-7 h-7 cursor-pointer '>
+            <input
+              type='checkbox'
+              className={classNames(
+                ' cursor-pointer appearance-none w-7 h-7 rounded-full bg-no-repeat bg-center border-2 border-[#2656CD] checked:bg-[#2656CD]',
+                'checked:border-none hover:bg-[#2656CD] transition-all duration-200 ease-in-out',
+              )}
+              onChange={onToggleCheck}
+            />
+            {agreementInfo.isChecked && (
+              <img
+                className='absolute w-3.5 h-3.5'
+                src={check_logo}
+              />
+            )}
+          </label>
+          <h2 className='text-[#162C63] text-2xl font-bold'>
+            {agreementInfo.title}
+          </h2>
+        </div>
+        <button
+          className='pr-7'
+          type='button'
+          onClick={() => onToggleContent()}>
+          <img
+            className={`w-11 h-11 cursor-pointer transition-transform duration-200 ease-in-out ${
+              agreementInfo.isExpanded ? 'rotate-180' : 'rotate-0'
+            }`}
+            src={toggle_up}
+            alt='토글 버튼'
+          />
+        </button>
+      </div>
+      {/* 토글시 보이는 부분 */}
+      {agreementInfo.isExpanded && (
+        <li
+          className='absolute z-[-1]  top-0 left-0 w-full h-54  pt-[82px]  rounded-2xl bg-[#FFFFFF] 
+      shadow-[0px_4px_4px_0px_rgba(78,122,207,0.15)] px-10 '>
+          <p className='w-full h-full scrollbar text-[#162C63] pt-6 pr-4 pb-12 whitespace-pre-line '>
+            {agreementInfo.content}
+          </p>
+        </li>
+      )}
+    </ul>
+  );
+}
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import background_img from '@assets/roome-background-img.png';
+import SocialTab from './SocialTab';
+import AgreeTab from './AgreeTab';
+import { agreeItems } from '@constants/login';
+
+export default function LoginPage() {
+  const [agreement, setAgreement] = useState<Agreement[]>(agreeItems);
+
+  const handleChangeAgreement = (item: Agreement[]) => {
+    setAgreement(item);
+  };
+
+  return (
+    <div
+      style={{ backgroundImage: `url(${background_img})` }}
+      className={`bg-cover bg-center bg-no-repeat h-screen item-middle `}>
+      <div
+        className='w-full h-[740px] max-w-[1134px] p-4.5 rounded-[36px] border-2 border-[#FCF7FD] bg-[#FCF7FD33]   backdrop-blur-lg
+       shadow-[inset_0px_0px_40px_10px_rgba(152,200,228,0.30),_0px_10px_20px_0px_rgba(62,80,125,0.20)]'>
+        {agreement[0].isChecked && agreement[1].isChecked ? (
+          <SocialTab />
+        ) : (
+          <AgreeTab
+            agreement={agreement}
+            onChangeAgreement={handleChangeAgreement}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
