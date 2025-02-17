e97a00d72fe80fc973dcbd9499b0ce98f1a12742
miseullang <miseullang@gmail.com>
2025-02-17 16:09:09 +0900

    :memo:Chore: title 변경 및 favicon 변경

--- src/pages/bookcase/BookCasePage.tsx
+++ src/pages/bookcase/BookCasePage.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { SearchModal } from '@components/search-modal/SearchModal';
+import { useState } from 'react';
+import addBookIcon from '@/assets/book/add-book-icon.svg';
 
 const BookCasePage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div>BookCasePage</div>
-  )
+    <div className='h-screen bg-blue-100'>
+      BookCasePage
+      <button
+        className='p-5 bg-white rounded-full cursor-pointer'
+        onClick={() => setIsModalOpen(true)}>
+        <img
+          src={addBookIcon}
+          alt='도서 추가하기'
+        />
+      </button>
+      {isModalOpen && (
+        <SearchModal
+          title='책장에 담을 책 찾기'
+          type='BOOK'
+          onClose={() => setIsModalOpen(false)}
+        />
+      )}
+    </div>
+  );
 }
 
 export default BookCasePage
