630a3406b23bac4fbaa72e3828a622f8550d8f25
miseullang <miseullang@gmail.com>
2025-02-22 12:37:36 +0900

    :sparkles:Feature: 서평 조회 결과에 따라 도서 상세 페이지 분기 처리

--- src/pages/book/BookPage.tsx
+++ src/pages/book/BookPage.tsx
@@ -2,10 +2,27 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import BookReviewEditor from '../book-editor/BookEditorPage';
 import BookReviewViewer from '../book-viewer/BookViewerPage';
+import { bookAPI } from '@/apis/book';
 
 const BookPage = () => {
-  const { bookId, userId } = useParams();
-  const [hasReview, setHasReview] = useState(true);
+  const { bookId } = useParams();
+  const [hasReview, setHasReview] = useState(false);
+
+  useEffect(() => {
+    const checkReview = async () => {
+      try {
+        if (!bookId) return;
+
+        const review = await bookAPI.getReview(bookId);
+        setHasReview(!!review); // review가 있으면 true, 없으면 false
+      } catch (error) {
+        console.error('서평 조회 중 오류 발생:', error);
+        setHasReview(false);
+      }
+    };
+
+    checkReview();
+  }, [bookId]);
 
   return <>{hasReview ? <BookReviewViewer /> : <BookReviewEditor />}</>;
 };
