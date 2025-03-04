55b67816d0effa9bd53bf6eb28d796d50fac5568
choiboa <musamea99@gmail.com>
2025-03-04 16:22:15 +0900

    ✨Feature: 랭킹 프로필 리다이렉션 추가 및 데이터 수정

--- src/pages/main/components/TopRankingItem.tsx
+++ src/pages/main/components/TopRankingItem.tsx
@@ -3,6 +3,7 @@ import goldMedal from '@assets//rank//gold-medal.svg';
 import silverMedal from '@assets//rank//silver-medal.svg';
 import blueFoot from '@assets/rank/blue-footprint.svg';
 import pinkFoot from '@assets/rank/pink-footprint.svg';
+import { Link } from 'react-router-dom';
 
 const medalImages = {
   1: goldMedal,
@@ -14,9 +15,21 @@ export default function TopRankingItem({ user }) {
   return (
     <div
       className={`
-        ${user.rank === 1 ? 'order-2 w-[80px] h-[110px] @2xl:w-[100px] @2xl:h-32' : ''}
-        ${user.rank === 2 ? 'order-1 w-[70px] h-[96px] @2xl:w-[85px] @2xl:h-28' : ''}
-        ${user.rank === 3 ? 'order-3 w-[70px] h-[96px] @2xl:w-[85px] @2xl:h-28' : ''}
+        ${
+          user.rank === 1
+            ? 'order-2 w-[80px] h-[110px] @2xl:w-[100px] @2xl:h-32'
+            : ''
+        }
+        ${
+          user.rank === 2
+            ? 'order-1 w-[70px] h-[96px] @2xl:w-[85px] @2xl:h-28'
+            : ''
+        }
+        ${
+          user.rank === 3
+            ? 'order-3 w-[70px] h-[96px] @2xl:w-[85px] @2xl:h-28'
+            : ''
+        }
         drop-shadow-logo rounded-b-xl bg-white flex justify-center items-center relative @container
     `}>
       {/* 메달 이미지 */}
@@ -30,33 +43,48 @@ export default function TopRankingItem({ user }) {
       />
 
       {/* 사용자 정보 */}
-      <div className='flex flex-col items-center gap-1'>
-        <img
-          className={`
-            ${user.rank === 1 ? 'w-9 @2xl:w-12' : 'w-7.5 @2xl:w-10'} rounded-full`}
-          src={user.profileImage}
-          alt={user.nickname}
-        />
-        <p className={`font-semibold text-xs @2xl:text-sm text-[#162C63] mt-1 max-w-[60px] truncate
-          ${user.nickname.length > 5 ? 'text-[10px] @2xl:text-xs' : 'text-xs @2xl:text-sm'}
-          `}>
-          {user.nickname}
-        </p>
-
-        {/* 방문자 수 */}
-        <div className='flex flex-row items-center gap-0.5'>
-          <img 
-          className={`${user.rank === 1 ? 'w-2.5 @2xl:w-3' : 'w-2 @2xl:w-2.5'}`} 
-          src={user.rank === 1 ? pinkFoot : blueFoot} />
-          <p
+      <Link to={`/profile/${user.userId}`}>
+        <div className='flex flex-col items-center gap-1'>
+          <img
             className={`
-              ${user.rank === 1 ? 'font-semibold text-xs @2xl:text-sm text-[#BA4B87]' : 'font-medium text-[10px] @2xl:text-xs text-[#4B6BBA]'}
-            `}
-            >
-            {user.score}
+            ${
+              user.rank === 1 ? 'w-9 @2xl:w-12' : 'w-7.5 @2xl:w-10'
+            } rounded-full`}
+            src={user.profileImage}
+            alt={user.nickname}
+          />
+          <p
+            className={`font-semibold text-xs @2xl:text-sm text-[#162C63] mt-1 max-w-[60px] truncate
+          ${
+            user.nickname.length > 5
+              ? 'text-[10px] @2xl:text-xs'
+              : 'text-xs @2xl:text-sm'
+          }
+          `}>
+            {user.nickname}
           </p>
+
+          {/* 방문자 수 */}
+          <div className='flex flex-row items-center gap-0.5'>
+            <img
+              className={`${
+                user.rank === 1 ? 'w-2.5 @2xl:w-3' : 'w-2 @2xl:w-2.5'
+              }`}
+              src={user.rank === 1 ? pinkFoot : blueFoot}
+            />
+            <p
+              className={`
+              ${
+                user.rank === 1
+                  ? 'font-semibold text-xs @2xl:text-sm text-[#BA4B87]'
+                  : 'font-medium text-[10px] @2xl:text-xs text-[#4B6BBA]'
+              }
+              `}>
+              {user.score}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
