5c870624dce21a1dbd5966ffddbe7af7977c57e2
hyunw00 <khwland090@gmail.com>
2025-03-06 10:47:51 +0900

    ✨ feat: point 내역 페이지 기능 구현

--- src/pages/point/PointPage.tsx
+++ src/pages/point/PointPage.tsx
@@ -1,8 +1,84 @@
+import { useUserStore } from '@/store/useUserStore';
+import { getPointBalance, getPointHistory } from '@apis/point';
 import receipt from '@assets/point/receipt.svg';
 import LayeredButton from '@components/LayeredButton';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
+import PointHistory from './components/PointHistory';
 
 export default function PointPage() {
+  const navigate = useNavigate();
+  const [pointBalance, setPointBalance] = useState(0);
+
+  const userId = Number(useParams().userId);
+  const myUserId = useUserStore((state) => state.user).userId;
+
+  const { ref, inView } = useInView();
+
+  // useInfiniteQuery
+  const { data, isLoading, isFetching, hasNextPage, fetchNextPage } =
+    useInfiniteQuery({
+      queryKey: ['points', userId],
+      queryFn: async ({ pageParam }) => {
+        return fetchPointsHistory(pageParam);
+      },
+      getNextPageParam: (lastPage) => {
+        if (lastPage.nextCursor > lastPage.lastId) {
+          return lastPage.nextCursor;
+        }
+        return undefined;
+      },
+      initialPageParam: 0,
+      staleTime: 1000 * 60 * 5, // 5분
+    });
+
+  useEffect(() => {
+    const fetchPointBalance = async () => {
+      try {
+        const { balance } = await getPointBalance();
+        setPointBalance(balance);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPointBalance();
+  }, []);
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+
+  const fetchPointsHistory = async (cursor: number) => {
+    const result = await getPointHistory(5, cursor);
+
+    console.log(result);
+
+    const history = result.history;
+
+    // 날짜별로 묶어서 배열화
+    const filteredByDates = history.reduce(
+      (prev: PointHistory, cur: PointHistory) => {
+        const dateKey = cur.dateTime.split(' ')[0];
+        if (!prev[dateKey]) {
+          prev[dateKey] = [];
+        }
+        prev[dateKey].push(cur);
+        return prev;
+      },
+      {},
+    );
+    return { ...result, history: [...Object.entries(filteredByDates)] };
+  };
+
+  if (userId !== myUserId) {
+    navigate(`/profile/${userId}`);
+  }
+
   return (
     <div className='w-full h-screen main-background'>
       {/* 메인 배경 */}
@@ -22,42 +98,11 @@ export default function PointPage() {
           <h1 className='absolute top-10 left-38 text-[#3E507D] text-[30px] font-bold'>
             Point Receipt
           </h1>
-
-          {/* 보여줄 포인트 내역 */}
-          <section className='absolute inset-0 w-full h-full overflow-auto pt-28 pl-13   pr-16'>
-            {/* 날짜별 적립, 차감 */}
-            <div className='flex flex-col gap-5 py-6 '>
-              <p className='text-right text-[#162C6399]'>2025.03.03.</p>
-              <ul className='flex flex-col gap-6'>
-                <li className='flex  items-center  text-[16px] w-full'>
-                  <span>적립</span>
-                  <span className='ml-30'>출석체크</span>
-                  <span className='ml-auto text-blue-700'>+400P</span>
-                </li>
-                <li className='flex items-center  text-[16px]'>
-                  <span>차감</span>
-                  <span className='ml-30'>댓글삭제</span>
-                  <span className='ml-auto'>-20P</span>
-                </li>
-              </ul>
-            </div>
-
-            <div className='flex flex-col gap-5 py-6 '>
-              <p className='text-right text-[#162C6399]'>2025.03.02.</p>
-              <ul className='flex flex-col gap-6'>
-                <li className='flex  items-center  text-[16px] w-full'>
-                  <span>적립</span>
-                  <span className='ml-30'>출석체크</span>
-                  <span className='ml-auto text-blue-700'>+200P</span>
-                </li>
-                <li className='flex items-center  text-[16px]'>
-                  <span>차감</span>
-                  <span className='ml-30'>댓글삭제</span>
-                  <span className='ml-auto'>-50P</span>
-                </li>
-              </ul>
-            </div>
-          </section>
+          <PointHistory
+            data={data}
+            isFetching={isFetching}
+            ref={ref}
+          />
 
           <p className='absolute bottom-33 left-12 text-[#162C63] text-[16px]'>
             포인트 잔고
@@ -65,13 +110,15 @@ export default function PointPage() {
 
           {/* 포인트 */}
           <p className='absolute bottom-33 right-17 text-[#162C63] text-[16px]'>
-            400p
+            {pointBalance}
           </p>
           {/* 충전 버튼 */}
+
           <div className='absolute bottom-13 left-1/2 -translate-x-1/2'>
             <LayeredButton
               theme='blue'
-              className=' text-[18px] font-bold w-[202px] h-[50px]'>
+              className=' text-[18px] font-bold w-[202px] h-[50px]'
+              disabled={isLoading}>
               포인트 충전하기
             </LayeredButton>
           </div>
