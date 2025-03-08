95e1675332ffe3b2ce29d6a27841b71706e0abc8
hyunw00 <khwland090@gmail.com>
2025-03-08 15:03:29 +0900

    💄 style: 로그인 페이지 디자인 수정

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
       style={{ backgroundImage: `url(${background_img})` }}
       className={`bg-cover bg-center bg-no-repeat h-screen item-middle `}>
       <div
-        className='w-full h-[740px] max-w-[1134px] p-4.5 rounded-[36px] border-2 border-[#FCF7FD] bg-[#FCF7FD33]   backdrop-blur-lg
+        className='w-full h-[650px]  max-w-[1134px] p-4.5 rounded-[36px] border-2 border-[#FCF7FD] bg-[#FCF7FD33]   backdrop-blur-lg
        shadow-[inset_0px_0px_40px_10px_rgba(152,200,228,0.30),_0px_10px_20px_0px_rgba(62,80,125,0.20)]'>
         {agreement[0].isChecked && agreement[1].isChecked ? (
           <SocialTab />
