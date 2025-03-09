f5fd2b2da69b8467e23b1a03be3955a17fa9b9b0
miseullang <miseullang@gmail.com>
2025-03-09 13:36:49 +0900

    :rotating_light:Fix: 도서 관련 배포 환경 서평 데이터 null 체크를 위한 옵셔널 체이닝 추가

--- src/pages/book-viewer/components/BookHeader.tsx
+++ src/pages/book-viewer/components/BookHeader.tsx
@@ -24,7 +24,7 @@ export const BookHeader = ({
   <div className='py-12 item-between px-14'>
     <h1
       className={`mb-8 font-semibold transition-all duration-300 ${
-        title.length > 10 ? 'text-4xl' : 'text-6xl'
+        title?.length > 10 ? 'text-4xl' : 'text-6xl'
       }`}
       style={{ color: colors.primary }}>
       {title}
