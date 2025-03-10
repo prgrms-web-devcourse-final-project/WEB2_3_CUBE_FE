047c13f6f3485401eab53afa774ca0c10c469af0
miseullang <miseullang@gmail.com>
2025-03-10 11:23:09 +0900

    :memo:Chore: 프로필카드 유저 추천 문구 변경

--- src/pages/profile-card/components/RecommendedUserList.tsx
+++ src/pages/profile-card/components/RecommendedUserList.tsx
@@ -8,7 +8,7 @@ const RecommendedUserList = ({ users }: RecommendedUserListProps) => {
   return (
     <div className='flex flex-col w-full gap-4 bg-[#F9F4FB] rounded-2xl px-8 py-4'>
       <h3 className='text-[#224DBA] text-lg font-bold text-center'>
-        나와 취향이 비슷한 유저
+        취향이 비슷한 유저
       </h3>
       <ul className='w-full gap-3 item-middle'>
         {users.length > 0 ? (
