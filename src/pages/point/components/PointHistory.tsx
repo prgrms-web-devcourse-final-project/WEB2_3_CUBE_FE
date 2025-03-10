d4a46fa07701eee4080ce39eeab05f636e2b4bd3
choiboa <musamea99@gmail.com>
2025-03-10 10:48:31 +0900

    💄 style: 영수증 퍼블리싱 수정

--- src/pages/point/components/PointHistory.tsx
+++ src/pages/point/components/PointHistory.tsx
@@ -13,26 +13,26 @@ const PointHistory = forwardRef<HTMLDivElement, PointHistoryProps>(
 
     return (
       <section
-        className='translate-y-10 w-full h-[370px] overflow-y-auto  point-scrollbar 
+        className='translate-y-5 w-full h-[360px] overflow-y-auto  point-scrollbar 
         border-t-2 border-b-2 border-dashed border-[#B7C7EA] relative
-      flex flex-col gap-11 py-5'>
+      flex flex-col gap-7 py-3'>
         {/* 날짜별 적립, 차감 */}
         {data?.pages.map((page: Points, index: number) => (
           <React.Fragment key={index}>
             {page.history.map((data: FilteredHistory, index: number) => (
               <div
                 key={index}
-                className='flex flex-col gap-5  '>
-                <p className='text-right text-[#162C6399] text-[14px]'>
+                className='flex flex-col gap-1'>
+                <p className='text-right text-[#162C6399] text-xs'>
                   {toKoreanDate(data[0])}
                 </p>
-                <ul className='flex flex-col gap-6'>
+                <ul className='flex flex-col gap-4'>
                   {data[1].map((point: PointHistory) => (
                     <li
                       key={point.id}
-                      className='flex  items-center  text-[16px] w-full'>
-                      <span>{point.type}</span>
-                      <span className='ml-25'>{PointReason[point.reason]}</span>
+                      className='flex items-center font-medium w-full'>
+                      <span className='text-sm text-[#3E507D]'>{point.type}</span>
+                      <span className='ml-20 text-[#162C63]'>{PointReason[point.reason]}</span>
                       <span
                         className={`ml-auto ${
                           point.type === '적립'
